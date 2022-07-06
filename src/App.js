7d63c0cb1de13fe7ef83f17e2b6c33824632998c
Pilli3800 <l.alvarezaguilar448@gmail.com>
2022-07-05 19:58:36 -0500

    Correcion Navbar - Terminado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx"
-import Inicio from "./pages/Inicio"
-import Sobrenosotros from "./pages/Sobrenosotros"
-import Contacto from "./pages/Contacto"
-import Ingresar from "./pages/Ingresar"
+import "./App.css";
+import {Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Inicio from "./pages/Inicio";
+import Sobrenosotros from "./pages/Sobrenosotros";
+import Contacto from "./pages/Contacto";
+import Ingresar from "./pages/Ingresar";
 function App() {
   return (
-    <Router>
-      <Navbar />
+    <>
+    <Navbar />
       <Routes>
-        <Route path='/' element={<Inicio/>} />
-        <Route path='/sobrenosotros' element={<Sobrenosotros/>} />
-        <Route path='/contacto' element={<Contacto/>} />
-        <Route path='/ingresar' element={<Ingresar/>} />
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobrenosotros" element={<Sobrenosotros />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/ingresar" element={<Ingresar />} />
       </Routes>
-    </Router>
+    </>
   );
 }
-  
+
 export default App;
---
+++ src/Layout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from './App'
+
+const Layout = () => {
+  return <App />;
+};
+
+export default Layout;
