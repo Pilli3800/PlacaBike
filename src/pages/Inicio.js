1aeb58f0c4871e6073b4e5c2de86d4a7c35ee50c
Pilli3800 <l.alvarezaguilar448@gmail.com>
2022-07-08 11:09:59 -0500

    Section Hero terminada e inicia Cards

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,20 +1,39 @@
 import React from "react";
 import "./Inicio.css";
-import bicycle from '../resources/img/bicycle.png';
+import bicycle from "../resources/img/bicycle.png";
+import chains from "../resources/img/chains.png";
 
 const Inicio = () => {
   return (
     <main>
       <section className="section-hero">
         <div className="hero-container">
-          <h1>
-            Es hora de sacar la <span className="whiteSpan">PLACA</span> para tu{" "}
-            <span className="darkSpan">BIKE</span>
-          </h1>
-          <p>lnern gjrengjnrejgnrengrt</p>
-        </div>
-        <div className="hero-container-img">
+          <div className="hero-container-text">
+            <h1>
+              Es hora de sacar la <span className="whiteSpan">PLACA</span> para
+              tu <span className="darkSpan">BIKE</span>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+              venenatis, quam et sodales pharetra, dolor ante blandit neque, eu
+              finibus dolor velit at quam.{" "}
+            </p>
+          </div>
+          <div className="hero-container-img">
             <img className="bicycle" src={bicycle} alt="bicycle"></img>
+          </div>
+        </div>
+      </section>
+      <section className="section-cards">
+        <div className="cards-container">
+          <div className="cards-container-img">
+            <img className="chains" src={chains} alt="chains"></img>
+          </div>
+          <div className="cards-container-card">
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+          </div>
         </div>
       </section>
     </main>
