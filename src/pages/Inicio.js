b9c5c53cd71e731d8920b1a7654078a376a78657
Pilli3800 <l.alvarezaguilar448@gmail.com>
2022-07-07 22:41:09 -0500

    Avance Inicio - Hero
    Se añadio texto e imagen de bicicleta.

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Inicio.css";
+import bicycle from '../resources/img/bicycle.png';
 
 const Inicio = () => {
   return (
     <main>
       <section className="section-hero">
-        <h1>Inicio</h1>
+        <div className="hero-container">
+          <h1>
+            Es hora de sacar la <span className="whiteSpan">PLACA</span> para tu{" "}
+            <span className="darkSpan">BIKE</span>
+          </h1>
+          <p>lnern gjrengjnrejgnrengrt</p>
+        </div>
+        <div className="hero-container-img">
+            <img className="bicycle" src={bicycle} alt="bicycle"></img>
+        </div>
       </section>
     </main>
   );
