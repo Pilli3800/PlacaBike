847a8f3a4edb62e53eda36f5e1627dd25b54ac20
Pilli3800 <l.alvarezaguilar448@gmail.com>
2022-07-12 09:58:05 -0500

    Finished Ingresar, Fix Sobrenosotros

--- src/pages/Ingresar.js
+++ src/pages/Ingresar.js
@@ -1,11 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Ingresar.css";
+import AOS from "aos";
 
 const Ingresar = () => {
+  AOS.init();
+
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = data;
+
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: [e.target.value] });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
   return (
     <div className="ingresar-container">
-      <h1>Ingresar</h1>
-      <div className="login-form"></div>
+      <div className="login-form-container" data-aos="fade-down">
+        <div className="login-form-container-header">
+          <h3>Bienvenido</h3>
+        </div>
+        <form className="login-form" onSubmit={submitHandler}>
+          <input
+            className="login-input"
+            type="text"
+            name="username"
+            placeholder="Ingrese su DNI"
+            value={username}
+            onChange={changeHandler}
+          />
+          <br />
+          <input
+            className="login-input"
+            type="password"
+            name="password"
+            placeholder="Ingrese su contraseña"
+            value={password}
+            onChange={changeHandler}
+          />
+          <br />
+          <input type="submit" className="login-submit" name="submit" value="Ingresar" />
+        </form>
+        <div className="login-form-helper">
+          <p className="login-helper">
+            ¿No estas registrado aún?{" "}
+            <span className="spanHelperDark">Registrate</span>
+          </p>
+          <p className="login-helper">
+            ¿Olvidaste tu contraseña?{" "}
+            <span className="spanHelperDark">Recuperala</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
