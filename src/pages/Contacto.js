4f9b5ff43b03fb01fa53c348a8269e9b97d7a43a
Pilli3800 <l.alvarezaguilar448@gmail.com>
2022-07-15 13:51:40 -0500

    Contact start

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import Review from '../components/Review';
-const Contacto = () =>{
+import React from "react";
+import "./Contacto.css";
+const Contacto = () => {
   return (
-    <div>
-      <Review/>
+    <div className="section-contacto-container">
+      <div className="contacto-container">
+        <div className="contacto-text-container">
+          <h2 className="contacto-title">¿Quieres decirnos algo?</h2>
+          <p className="contacto-txt">Te dejamos nuestras redes sociales: </p>
+          <div className="contacto-icons-container">
+            <i class="fa-brands fa-facebook icon-social"></i>
+            <i class="fa-brands fa-whatsapp icon-social"></i>
+            <i class="fa-brands fa-instagram icon-social"></i>
+          </div>
+        </div>
+        <div className="contacto-form-container"></div>
+      </div>
     </div>
   );
-}
+};
 export default Contacto;
