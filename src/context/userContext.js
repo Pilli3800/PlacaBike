41a5f11bd1b8ed6d068a88bd561c67ca3abdbd4d
Pilli3800 <l.alvarezaguilar448@gmail.com>
2022-07-31 19:51:45 -0500

    Fix UserContext

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 
-const userContext = createContext({
+const UserContext = createContext({
   name: null,
 });
 
-export default userContext;
+export default UserContext;
