a13f92290524671827d35e8a7405633a09cbec58
Pilli3800 <l.alvarezaguilar448@gmail.com>
2022-07-31 19:49:18 -0500

    Delete unused component

--- src/user/routes/UserRoutes.jsx
+++ src/user/routes/UserRoutes.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbarauth from "../../ui/components/navbar-user-auth/Navbarauth";
 import { Routes, Route } from "react-router-dom";
-import Recovery from "../../pages/recovery/Recuperar";
 import Perfil from "../pages/auth/perfil/Perfil";
 import Dashboard from "../pages/auth/dashboard/Dashboard";
 import Bicicletas from "../pages/auth/bicicletas/Bicicletas";
